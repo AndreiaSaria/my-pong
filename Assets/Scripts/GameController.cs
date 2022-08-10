1e3e0f64b3d3b1c1bc7236f465bfa07cc635212f
Andréia Fernandez/Saria <92866182+andreia-fernandez-afterverse@users.noreply.github.com>
2022-08-10 15:10:34 -0300

    Removing comment on gamecontroller

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,6 +1,5 @@
 using UnityEngine;
 
-//Although it has state on the name its not a state of a state machine.
 public class GameController : MonoSingleton<GameController>
 {
     [SerializeField] private BallController _ball;
